4c5cc2399d6fae8b58b0676b6d387e175d9b8e76
Alessio Parma <alessio.parma@gmail.com>
2016-08-28 09:12:08 +0200

    v3.0.2

--- Platform Specific/Thrower.NETSTD11/Properties/AssemblyInfo.cs
+++ Platform Specific/Thrower.NETSTD11/Properties/AssemblyInfo.cs
@@ -47,7 +47,7 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.0.1")]
+[assembly: AssemblyFileVersion("3.0.2")]
 
 // Common Language Specification (CLS) compliance generally refers to the claim that CLS rules and
 // restrictions are being followed.
