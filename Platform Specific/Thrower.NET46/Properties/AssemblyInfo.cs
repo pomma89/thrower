32aef72f2a2f2c974b1bbb462a8aa76ab605993b
Alessio Parma <alessio.parma@gmail.com>
2017-01-27 17:43:25 +0100

    license to 2018

--- Platform Specific/Thrower.NET46/Properties/AssemblyInfo.cs
+++ Platform Specific/Thrower.NET46/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 //
 // Author(s): Alessio Parma <alessio.parma@gmail.com>
 //
-// Copyright (c) 2013-2016 Alessio Parma <alessio.parma@gmail.com>
+// Copyright (c) 2013-2018 Alessio Parma <alessio.parma@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 // associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -32,7 +32,7 @@ using System.Security;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PommaLabs")]
 [assembly: AssemblyProduct("PommaLabs.Thrower")]
-[assembly: AssemblyCopyright("Copyright (c) 2013-2016 Alessio Parma <alessio.parma@gmail.com>")]
+[assembly: AssemblyCopyright("Copyright (c) 2013-2018 Alessio Parma <alessio.parma@gmail.com>")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
