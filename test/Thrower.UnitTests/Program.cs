5616c7f346b440386fe3ea50af6781daeef89267
Alessio Parma <alessio.parma@gmail.com>
2017-01-29 17:03:45 +0100

    .net core experiments

---
+++ test/Thrower.UnitTests/Program.cs
@@ -0,0 +1,17 @@
+﻿using NUnitLite;
+using System.Reflection;
+
+namespace PommaLabs.Thrower.UnitTests
+{
+    internal static class Program
+    {
+        public static int Main(string[] args)
+        {
+#if NET40
+            return new AutoRun(typeof(Program).Assembly).Execute(args);
+#else
+            return new AutoRun(typeof(Program).GetTypeInfo().Assembly).Execute(args);
+#endif
+        }
+    }
+}
