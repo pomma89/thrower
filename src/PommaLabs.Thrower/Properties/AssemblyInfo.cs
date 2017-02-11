63ab8ef5f8c889ebcd31be37dc2ea26d612eed08
Alessio Parma <alessio.parma@gmail.com>
2017-02-11 15:58:48 +0100

    fully net std

---
+++ src/PommaLabs.Thrower/Properties/AssemblyInfo.cs
@@ -0,0 +1,52 @@
+﻿// File name: AssemblyInfo.cs
+//
+// Author(s): Alessio Parma <alessio.parma@gmail.com>
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2013-2018 Alessio Parma <alessio.parma@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+// OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+[assembly: AssemblyVersion("4.0.0")]
+
+// General Information about an assembly is controlled through the following set of attributes.
+// Change these attribute values to modify the information associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PommaLabs")]
+[assembly: AssemblyProduct("PommaLabs.Thrower")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM components. If you
+// need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Common Language Specification (CLS) compliance generally refers to the claim that CLS rules and
+// restrictions are being followed.
+[assembly: CLSCompliant(true)]
+
+// Allows an assembly to be called by partially trusted code. Without this declaration, only fully
+// trusted callers are able to use the assembly.
+[assembly: AllowPartiallyTrustedCallers]
+
+// Added to improve unit testing.
+[assembly: InternalsVisibleTo("PommaLabs.Thrower.UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100efe22a8787b348e219d6b501b425d79f31502681d5a9f7d11030852e44e7ef2b29ddbc7dfcd6461fc3e67a6d7a186dea40535dad461679209a079a45440a99440bd292498b623e9a7fc5161c519c8d45b79fdfbd95a6e0ac5e211d5438d47b8635662d108448cb84bf1212983fab0010c68b41d5168bcbe5a59f9149974852c4")]
