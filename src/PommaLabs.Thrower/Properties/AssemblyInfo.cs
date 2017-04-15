32a881d8be90a51e6e3e0cb73292d5b5f79fe8f5
Alessio Parma <alessio.parma@gmail.com>
2017-04-15 20:45:36 +0200

    cfg cleanup

--- src/PommaLabs.Thrower/Properties/AssemblyInfo.cs
+++ src/PommaLabs.Thrower/Properties/AssemblyInfo.cs
@@ -26,14 +26,9 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security;
 
-[assembly: AssemblyVersion("4.0.0")]
-
 // General Information about an assembly is controlled through the following set of attributes.
 // Change these attribute values to modify the information associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("PommaLabs")]
-[assembly: AssemblyProduct("PommaLabs.Thrower")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("4.0.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible to COM components. If you
 // need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
